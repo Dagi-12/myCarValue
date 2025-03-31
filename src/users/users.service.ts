8729f5c42ec89732542428f53d46a00bd0912991
Dagim <dagimawiengidawork371@gmail.com>
2025-03-31 08:12:53 +0300

    user services commomn logic set

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,15 +2,35 @@ import { Injectable } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './users.entity';
+
 @Injectable()
 export class UsersService {
   constructor(@InjectRepository(User) private repo: Repository<User>) {}
+
   create(email: string, password: string) {
     const user = this.repo.create({ email, password });
     return this.repo.save(user);
   }
-  findOne(id: number) {}
-  find() {}
-  update() {}
-  remove() {}
+
+  findOne(id: number) {
+    return this.repo.findOne({ where: { id } });
+  }
+  find(email: string) {
+    return this.repo.find({ where: { email } });
+  }
+  async update(id: number, attributes: Partial<User>) {
+    const user = await this.findOne(id);
+    if (!user) {
+      throw new Error('user not found');
+    }
+    Object.assign(user, attributes);
+    return this.repo.save(user);
+  }
+  async remove(id: number) {
+    const user = await this.findOne(id);
+    if (!user) {
+      return new Error('user not found');
+    }
+    return this.repo.remove(user);
+  }
 }

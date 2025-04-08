f93b7ae8edeca9690f449bc6a4b326fb8a7abe29
Dagim <dagimawiengidawork371@gmail.com>
2025-04-08 12:03:44 +0300

    report approval set

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,11 +1,19 @@
-import { Controller, Post, Body, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  UseGuards,
+  Patch,
+  Param,
+} from '@nestjs/common';
 import { CreateReportDto } from './dtos/create-report.dto';
 import { ReportsService } from './reports.service';
 import { AuthGuard } from 'src/guards/auth.guards';
 import { CurrentUser } from '../users/decorators/current-user.decorator';
 import { User } from 'src/users/users.entity';
 import { ReportDto } from './dtos/report.dto';
-import {serialize} from '../interceptors/serialize.interceptor'
+import { serialize } from '../interceptors/serialize.interceptor';
+import { ApproveReportDto } from './dtos/approve-user.dto';
 @Controller('reports')
 export class ReportsController {
   constructor(private reportService: ReportsService) {}
@@ -15,5 +23,10 @@ export class ReportsController {
   @serialize(ReportDto)
   createReport(@Body() body: CreateReportDto, @CurrentUser() user: User) {
     return this.reportService.create(body, user);
+  }
+
+  @Patch('/:id')
+  approveReport(@Param('id') id: string, @Body() body: ApproveReportDto) {
+    return this.reportService.changeApproval(id, body.approve);
   }
 }

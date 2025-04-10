c4209541d3984ae286ead0e16d259f75f0a438ab
Dagim <dagimawiengidawork371@gmail.com>
2025-04-10 11:30:34 +0300

    current user set on the express request

--- src/users/middlewares/current-user.middleware.ts
+++ src/users/middlewares/current-user.middleware.ts
@@ -1,6 +1,14 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
 import { UsersService } from '../users.service';
+import { User } from '../users.entity';
+declare global {
+  namespace Express {
+    interface Request {
+      currentUser?: User | null;
+    }
+  }
+}
 
 @Injectable()
 export class CurrentUserMiddleware implements NestMiddleware {
@@ -9,7 +17,6 @@ export class CurrentUserMiddleware implements NestMiddleware {
     const { userId } = req.session || {};
     if (userId) {
       const user = await this.usersService.findOne(userId);
-      //@ts-ignore
       req.currentUser = user;
     }
     next();
